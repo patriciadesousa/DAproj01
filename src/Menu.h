dd25a5e159a18c8c95e17e1429c6ebb8dff72efe
up202108044 <up202108044@up.pt>
2023-04-02 21:22:42 +0100

    Created Graph Structure and Read Files done (may have bugs)

---
+++ src/Menu.h
@@ -0,0 +1,18 @@
+#ifndef _MENU_H
+#define _MENU_H
+
+#include "iostream"
+
+using namespace std;
+
+class Menu {
+public:
+    void appMenu();
+    void dataInterfaceMenu();
+    void basicServiceMetricMenu();
+    void operationCostMenu();
+    void reliabilityAndSensitivityMenu();
+};
+
+
+#endif //_MENU_H

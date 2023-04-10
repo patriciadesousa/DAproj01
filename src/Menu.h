480f67b6a59fa6a78560fdd7907216fc12d05378
patriciadesousa <114480941+patriciadesousa@users.noreply.github.com>
2023-04-10 21:20:57 +0100

    Update Menu.h

--- src/Menu.h
+++ src/Menu.h
@@ -1,11 +1,13 @@
 #ifndef _MENU_H
 #define _MENU_H
+#include "T2.h"
 
 #include "iostream"
 
 using namespace std;
 
 class Menu {
+    T2 t;
 public:
     void appMenu();
     void dataInterfaceMenu();
